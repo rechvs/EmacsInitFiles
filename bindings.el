0e4df9f74388783e550e260bf0354f2f3a3b5821
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-07-23 16:55:07 +0200

    “bindings.el”: added binding for “my-find-region-or-at-point”.

--- bindings.el
+++ bindings.el
@@ -1,12 +1,14 @@
-(global-set-key (kbd "C-x C-y") 'my-recenter)
-
-(global-set-key (kbd "C-e") 'my-move-end-of-line)
+(global-set-key (kbd "C-x C-ö") 'my-find-region-or-at-point)
 
 (global-set-key (kbd "M-ö") 'my-immediately-switch-to-buffer)
 
-(global-set-key (kbd "<f5>") 'my-revert-buffer-without-confirmation)
+(global-set-key (kbd "C-e") 'my-move-end-of-line)
 
 (global-set-key (kbd "C-<tab>") 'my-other-window)
+
+(global-set-key (kbd "C-x C-y") 'my-recenter)
+
+(global-set-key (kbd "<f5>") 'my-revert-buffer-without-confirmation)
 
 (global-set-key (kbd "M-ä") 'my-switch-windows)
 
