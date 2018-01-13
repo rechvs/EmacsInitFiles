22337d5f6d91faf0de75f4d32d1c31d3587c7031
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-01-13 19:26:33 +0100

    “function_calls.el“: added comments.

--- function_calls.el
+++ function_calls.el
@@ -1,5 +1,6 @@
+;; Visit all files mentioned in "my-files-to-visit-at-startup", then visit the file mentioned in "initial-buffer-choice".
 (my-visit-multiple-files my-files-to-visit-at-startup)
-
 (find-file initial-buffer-choice)
 
+;; Activate Openwith mode.
 (openwith-mode)
