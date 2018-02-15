1b8e8e2e1db66ed0d8640531b8fd1c53efeff104
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-02-15 17:16:28 +0100

    “lists.el”: added Org elpa to “package-archives”; added “openwith” file handerl to “mm-inhibit-file-name-handlers”.

--- lists.el
+++ lists.el
@@ -6,5 +6,10 @@
 (add-to-list 'auto-mode-alist '("\\.bbx\\'" . latex-mode))
 (add-to-list 'auto-mode-alist '("\\.cbx\\'" . latex-mode))
 
+;; Prevent "openwith" from interfering with email attachments (see https://www.emacswiki.org/emacs/OpenWith).
+(add-to-list  'mm-inhibit-file-name-handlers 'openwith-file-handler)
+
 (add-to-list 'package-archives
 	   '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	   '("org" . "https://orgmode.org/elpa/"))
