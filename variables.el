443c9f564dcd6eb2b4fd00680903edd81b8fd22a
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-03-25 14:01:27 +0200

    “variables.el”: reorganized code; added variables pertaining to “my-man”. “variables/my-man-known-programs-only.el”; “variables/my-man-known-programs-plus-sections.el”: started tracking.

--- variables.el
+++ variables.el
@@ -1,3 +1,7 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Settings NOT involving "defcustom" or "defvar". ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Enable certain commands which are disabled by default. See Emacs info "Disabling".
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'downcase-region 'disabled nil)
@@ -11,7 +15,7 @@
 (put 'TeX-command-buffer 'disabled t)
 (put 'save-buffers-kill-terminal 'disabled t)
 
-;; add `/usr/local/texlive/2017/bin/i386-linux´ to `PATH´ so AUCTeX finds it (http://tex.stackexchange.com/questions/24510/pdflatex-fails-within-emacs-app-but-works-in-terminal):
+;; Add "/usr/local/texlive/2017/bin/i386-linux" to "PATH" so AUCTeX finds it (http://tex.stackexchange.com/questions/24510/pdflatex-fails-within-emacs-app-but-works-in-terminal).
 (setenv "PATH"
         (concat
          "/usr/local/texlive/2017/bin/i386-linux"
@@ -19,30 +23,7 @@
 
          (getenv "PATH")))
 
-(defcustom my-back-to-indentation-skip-chars
-  " "
-  "Regexp-like string containing the characters at the beginning of a line to be skipped by `my-back-to-indentation'. See `skip-chars-forward' for the string syntax."
-  :type '(string))
-(make-variable-buffer-local 'my-back-to-indentation-skip-chars)
-
-;; define a customizable variable `my-files-to-visit-at-startup´:
-(defcustom my-files-to-visit-at-startup
-  '("/home/renke/Privat/Privat.org")
-  "A list of paths to files which ought to be visited at startup."
-  :type '(repeat file))
-
-;; Define a non-customizable variable "my-Emacs-issue-index" which holds the index of the next Emacs issue as an integer.
-(defvar my-Emacs-issue-index
-  200
-  "The index of the next private Emacs issue.")
-
-;; Define a customizable variable "my-find-region-or-at-point-delim-chars" which is used as the regexp of delimiting characters by "my-find-region-or-at-point".
-(defcustom my-find-region-or-at-point-delim-chars
-  '"[][{}():\"'„“”][:space:]"
-  "A regexp-like string of characters used as delimiting characters by `my-find-region-or-at-point' (see argument STRING of `skip-chars-forward')."
-  :type '(string))
-
-;; add `parencite´ and `textcite´ to list of symbols known in style `article´ and dialect `LaTeX-dialect´ (s. AUCTeX info 5.6.2) such that AUCTeX can ask for an optional argument:
+;; Add "parencite" and "textcite" to list of symbols known in style "article" and dialect "LaTeX-dialect" (s. AUCTeX info 5.6.2) such that AUCTeX can ask for an optional argument.
 (eval-after-load "latex"
   '(progn
      (TeX-add-style-hook
@@ -57,13 +38,55 @@
 	 TeX-arg-cite)))
       LaTeX-dialect)))
 
-;; define a non-customizable variable `my-immediately-switch-to-buffer-counter´:
+;; Load certain variables whose standard value are too long to include directly in this file.
+(load "~/.emacs.d/init-files/variables/my-man-known-programs-only.el" t t)
+(load "~/.emacs.d/init-files/variables/my-man-known-programs-plus-sections.el" t t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Settings involving "defcustom" or "defvar". ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defcustom my-back-to-indentation-skip-chars
+  " "
+  "Regexp-like string containing the characters at the beginning of a line to be skipped by `my-back-to-indentation'. See `skip-chars-forward' for the string syntax."
+  :type '(string))
+(make-variable-buffer-local 'my-back-to-indentation-skip-chars)
+
+(defvar my-Emacs-issue-index
+  200
+  "The index of the next private Emacs issue.")
+
+(defcustom my-files-to-visit-at-startup
+  '("/home/renke/Privat/Privat.org")
+  "A list of paths to files which ought to be visited at startup."
+  :type '(repeat file))
+
+(defcustom my-find-region-or-at-point-delim-chars
+  '"[][{}():\"'„“”][:space:]"
+  "A regexp-like string of characters used as delimiting characters by `my-find-region-or-at-point' (see argument STRING of `skip-chars-forward')."
+  :type '(string))
+
 (defvar my-immediately-switch-to-buffer-counter
   0
   "Number of buffers in `buffer-list' to skip by `my-immediately-switch-to-buffer'.") 
 
-;; define a customizable variable `my-immediately-switch-to-buffer-excluded-buffers´:
 (defcustom my-immediately-switch-to-buffer-excluded-buffers
   '("\\*Minibuf-+[0-9]*[0-9]\\*")
   "A list containing buffer names as regular expressions which should not be switched to by `my-immediately-switch-to-buffer'."
   :type '(repeat regexp))
+
+(defvar my-man-input-hist
+  nil
+  "List of man pages invoked by the user.")
+
+(defvar my-man-known-programs-skip-chars-string
+  "+\\-.0123456789:@ABCDEFGHIJKLMNOPQRSTUVWXYZ[_abcdefghijklmnopqrstuvwxyz"
+  "String covering all characters of all program names known to man.") 
+
+(defvar my-man-known-sections-only
+  (list "1" "1p" "1ssl" "2" "3" "3am" "3caca" "3perl" "3pm" "3readline" "3ssl" "3tiff" "4" "5" "5ssl" "6" "6x" "7" "7ssl" "8")
+  "List of all sections (without programs) known to man.")
+
+(defvar my-man-known-sections-regexp
+  "\\(\\(1\\)\\|\\(1p\\)\\|\\(1ssl\\)\\|\\(2\\)\\|\\(3\\)\\|\\(3am\\)\\|\\(3caca\\)\\|\\(3perl\\)\\|\\(3pm\\)\\|\\(3readline\\)\\|\\(3ssl\\)\\|\\(3tiff\\)\\|\\(4\\)\\|\\(5\\)\\|\\(5ssl\\)\\|\\(6\\)\\|\\(6x\\)\\|\\(7\\)\\|\\(7ssl\\)\\|\\(8\\)\\)"
+  "Regexp covering all sections known to man.")
