2449a0ccf8c051f12970dedcfd4018a811632904
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-03-31 19:03:41 +0200

    “function_definitions.el”: replaced defun “my-newline-at-80” with defun “my-newline-at”.

--- function_definitions.el
+++ function_definitions.el
@@ -482,23 +482,41 @@ string1)
 	  (goto-char eol)))
       (set-match-data match-data-old))))
 
-(defun my-newline-at-80 ()
-  "Insert newline character after column 80 or, if there is a nonwhitespace character in column 80, at the beginning of the last whitespace character sequence before column 80."
-  (interactive)
-  (save-excursion (move-end-of-line nil) (setq COLNR (current-column)))
-  (if (<= COLNR 80)
-      (next-line)
-    (progn
-      (move-to-column 80)
-      (setq CHARBEFORE (char-before)))
-    (if (eq CHARBEFORE ? )
-        (newline-and-indent)
-      (progn
-        (save-excursion (move-beginning-of-line nil) (setq LINEBEGINNING (point)))
-        (search-backward-regexp "[ ]" LINEBEGINNING)
-        (search-backward-regexp "[^ ]" LINEBEGINNING)
-        (forward-char)
-        (newline-and-indent)))))
+(defun my-newline-at (&optional col)
+  "Insert newline character after the last whitespace (space or tab) character
+before or in column COL or, if that is nil, column
+`my-newline-at-col'. Move point to next line. When called interactively,
+prefix argument is taken as COL. If prefix argument is non-nil, set
+`my-new-line-at-col' to its value."
+  (interactive "p")
+  (let* ((cur-col (current-column))
+         (cur-line-length (save-excursion (move-end-of-line nil) (current-column)))
+         display-message
+         (match-data-old (match-data))
+         (my-newline-at-col-old my-newline-at-col)
+         (target-col (if (and (numberp col) (or (and (not col) (not current-prefix-arg)) (and (= col 1) (not current-prefix-arg)))) my-newline-at-col col))
+         (display-message (concat "Target column " (prin1-to-string target-col))))
+    (unwind-protect
+        (progn
+	(if (and col (not (numberp col)))
+	    (error "Non-numeric column number"))
+	(if (and (numberp col) (< col 0))
+	    (error "Negative column number"))
+	(if (<= cur-line-length target-col)
+	    (next-line)
+	  (progn
+	    (move-to-column target-col)
+	    (if (not (looking-back "[ 	]" (1- (point))))
+	        (skip-chars-backward "^ 	" (point-at-bol)))
+	    (if (= (point) (point-at-bol))
+	        (error (concat "Target column number (" (number-to-string target-col) ") too low")))
+	    (newline)))
+	(if current-prefix-arg
+	    (progn (setq my-newline-at-col col)
+		 (setq display-message (concat display-message " (old was " (number-to-string my-newline-at-col-old) ")"))))
+	(display-message-or-buffer (concat display-message ".")))
+      (move-to-column cur-col)
+      (set-match-data match-data-old))))
 
 (defun my-org-confirm-babel-evaluate (lang body)
   (not (string= lang "R")))
