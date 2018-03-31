f574d936591c3de3e5c19ca40b6cbcada7416c4a
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-03-31 19:02:50 +0200

    “workbench.el” defun “my-newline-at”: improved handling of interactive and non-interactive calls; added errors.

--- workbench.el
+++ workbench.el
@@ -4,33 +4,38 @@
     "Target column for `my-newline-at'.")
   )
 
-(defun my-newline-at (pref-arg-raw pref-arg-num)
-  "Insert newline character after the last whitespace (space or tab)
-character before or in the column specified by the prefix argument or, if
-that is nil, by `my-newline-at-col'. Move point to next line. If prefix
-argument is non-nil, set `my-new-line-at-col' to its value."
-  (interactive "P\np")
+(defun my-newline-at (&optional col)
+  "Insert newline character after the last whitespace (space or tab) character
+before or in column COL or, if that is nil, column
+`my-newline-at-col'. Move point to next line. When called interactively,
+prefix argument is taken as COL. If prefix argument is non-nil, set
+`my-new-line-at-col' to its value."
+  (interactive "p")
   (let* ((cur-col (current-column))
          (cur-line-length (save-excursion (move-end-of-line nil) (current-column)))
          display-message
          (match-data-old (match-data))
          (my-newline-at-col-old my-newline-at-col)
-         (target-col (if pref-arg-raw pref-arg-num my-newline-at-col))
-         (display-message (concat "Target column " (number-to-string target-col))))
+         (target-col (if (and (numberp col) (or (and (not col) (not current-prefix-arg)) (and (= col 1) (not current-prefix-arg)))) my-newline-at-col col))
+         (display-message (concat "Target column " (prin1-to-string target-col))))
     (unwind-protect
         (progn
-	(if (< pref-arg-num 0)
-	    (error "Negative prefix argument"))
+	(if (and col (not (numberp col)))
+	    (error "Non-numeric column number"))
+	(if (and (numberp col) (< col 0))
+	    (error "Negative column number"))
 	(if (<= cur-line-length target-col)
 	    (next-line)
 	  (progn
 	    (move-to-column target-col)
 	    (if (not (looking-back "[ 	]" (1- (point))))
 	        (skip-chars-backward "^ 	" (point-at-bol)))
-	    (newline-and-indent)
-	    (move-to-column cur-col)
-	    (if pref-arg-raw
-	        (progn (setq my-newline-at-col pref-arg-num)
-		     (setq display-message (concat display-message " (old was " (number-to-string my-newline-at-col-old) ")"))))
-	    (display-message-or-buffer (concat display-message ".")))))
+	    (if (= (point) (point-at-bol))
+	        (error (concat "Target column number (" (number-to-string target-col) ") too low")))
+	    (newline)))
+	(if current-prefix-arg
+	    (progn (setq my-newline-at-col col)
+		 (setq display-message (concat display-message " (old was " (number-to-string my-newline-at-col-old) ")"))))
+	(display-message-or-buffer (concat display-message ".")))
+      (move-to-column cur-col)
       (set-match-data match-data-old))))
