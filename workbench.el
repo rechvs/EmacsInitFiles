0ef3ded8b456336ddc53843f7352e2c94cb91997
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-04-22 12:33:42 +0200

    defun “my-find-or-browse-region-or-at-point-complicated”: function now uses regexp “my-regexp-url-identifier” to discern URLs from filenames.

--- workbench.el
+++ workbench.el
@@ -1,25 +1,29 @@
-(defun my-find-region-or-at-point (&optional arg)
-  "If region is active, use text in region as the filename to visit. Otherwise use the text at point as the filename to visit. The characters delimiting the filename from surrounding text are set via  `my-find-region-or-at-point-delim-chars'.
-Without prefix argument ARG, a non-existent filename results in an error.
-An empty filename results in an error."
+(defcustom my-regexp-url-identifier
+  "^file://\\|^mailto:\\|^\\(ftp://\\|https?://\\|www\\.\\).*\\.org\\|\\.\\(com\\|de\\)"
+  "Regexp with which to identify a URL."
+  :type '(regexp))
+
+(defun my-find-or-browse-region-or-at-point-complicated (&optional arg)
+  "Use a string as a local filename or a URL which to visit or browse.
+If region is active, use text in region as the string; otherwise use text around point.
+The characters delimiting the string from surrounding text are set via `my-find-region-or-at-point-delim-chars'.
+`my-regexp-url-identifier' determines what constitutes a URL; every string not matching it is considered a local filename.
+Without prefix argument ARG, a non-existent local filename results in an error.
+An empty string results in an error."
   (interactive "P")
-  (let ((flnm (if (region-active-p)
-                  (buffer-substring-no-properties (region-beginning) (region-end))
-                (buffer-substring-no-properties
-                 (save-excursion (skip-chars-backward (concat "^" my-find-region-or-at-point-delim-chars) (line-beginning-position)) (point))
-                 (save-excursion (skip-chars-forward (concat "^" my-find-region-or-at-point-delim-chars) (line-end-position)) (point))))))
-    ;; If flnm is empty, message the user about it, but do not visit it.
-    (if (string= "" flnm)
-        (error "Empty filename")
-      ;; If arg is non-nil, deactivate region (if applicable) and visit the file.
-      (if arg
-          (progn
-            (if (region-active-p) (deactivate-mark t))
-            (find-file flnm))
-        ;; Else, if flnm exists, deactivate region (if applicable) and visit the file.
-        (if (file-exists-p flnm)
-            (progn
-              (if (region-active-p) (deactivate-mark t))
-              (find-file flnm))
-          ;; If flnm does not exist, signal an error.
-          (error "File %s does not exist" flnm))))))
+  (let* ((flnm (if (region-active-p)
+                   (buffer-substring-no-properties (region-beginning) (region-end))
+                 (buffer-substring-no-properties
+                  (save-excursion (skip-chars-backward (concat "^" my-find-region-or-at-point-delim-chars) (line-beginning-position)) (point))
+                  (save-excursion (skip-chars-forward (concat "^" my-find-region-or-at-point-delim-chars) (line-end-position)) (point)))))
+         (flnm-is-url (string-match my-regexp-url-identifier flnm))
+         (match-data-old (match-data)))
+    (unwind-protect
+        (if (string= "" flnm)
+            (error "Empty string")
+          (if (region-active-p) (deactivate-mark t))
+          (if arg
+              (if flnm-is-url (browse-url flnm) (find-file flnm))
+            (if (file-exists-p flnm) (if flnm-is-url (browse-url flnm) (find-file flnm))
+              (if flnm-is-url (browse-url flnm) (error "File %s does not exist" flnm)))))
+      (set-match-data match-data-old))))
