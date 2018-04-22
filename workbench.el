3e0502f23ac69a31c55559b8440ea1849dc3cf80
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-04-22 10:09:26 +0200

    defun “my-find-region-or-at-point”: the filename is now properly set via the varlist of the “(let...)” form; replaced plain messages with errors.

--- workbench.el
+++ workbench.el
@@ -1,33 +1,25 @@
 (defun my-find-region-or-at-point (&optional arg)
-  "If region is active, use text in region as the filename to visit. Otherwise use the text at point as the filename to visit. The characters delimiting the filename from surrounding text are set via  `my-find-region-or-at-point-delim-chars'. If it exists, the file is visited via `find-file'. An empty filename is
-ignored."
+  "If region is active, use text in region as the filename to visit. Otherwise use the text at point as the filename to visit. The characters delimiting the filename from surrounding text are set via  `my-find-region-or-at-point-delim-chars'.
+Without prefix argument ARG, a non-existent filename results in an error.
+An empty filename results in an error."
   (interactive "P")
-  (let (FILENAME) 
-    ;; If the region is active, obtain the filename from it.
-    (if (region-active-p)
-        (setq FILENAME (buffer-substring-no-properties (region-beginning) (region-end)))
-      ;; If the region is not active, obtain the filename by scanning for text at point enclosed in the delimiting characters.
-      (let* ((DELIMCHARS (concat "^" my-find-region-or-at-point-delim-chars))
-             (P1 (save-excursion (skip-chars-backward DELIMCHARS (line-beginning-position)) (point)))
-             (P2 (save-excursion (skip-chars-forward DELIMCHARS (line-end-position)) (point))))
-        (setq FILENAME (buffer-substring-no-properties P1 P2))))
-    ;; If FILENAME is empty, message the user about it, but do not visit it.
-    (if (string= "" FILENAME)
-        (message "Empty filename ignored.")
-      ;; If prefix argument ARG is non-nil, deactivate region (if applicable) and visit the file.
+  (let ((flnm (if (region-active-p)
+                  (buffer-substring-no-properties (region-beginning) (region-end))
+                (buffer-substring-no-properties
+                 (save-excursion (skip-chars-backward (concat "^" my-find-region-or-at-point-delim-chars) (line-beginning-position)) (point))
+                 (save-excursion (skip-chars-forward (concat "^" my-find-region-or-at-point-delim-chars) (line-end-position)) (point))))))
+    ;; If flnm is empty, message the user about it, but do not visit it.
+    (if (string= "" flnm)
+        (error "Empty filename")
+      ;; If arg is non-nil, deactivate region (if applicable) and visit the file.
       (if arg
           (progn
             (if (region-active-p) (deactivate-mark t))
-            (find-file FILENAME))
-        ;; Else, if FILENAME exists, deactivate region (if applicable) and visit the file.
-        (if (file-exists-p FILENAME)
+            (find-file flnm))
+        ;; Else, if flnm exists, deactivate region (if applicable) and visit the file.
+        (if (file-exists-p flnm)
             (progn
               (if (region-active-p) (deactivate-mark t))
-              ;; ...and visit FILENAME.
-              (find-file FILENAME))
-          ;; If FILENAME does not exist, message the user about it.
-          ;; In order to distinguish FILENAME from the rest of the message, we can use either quotation marks...
-          ;; (message "File \"%s\" does not exist." FILENAME)))))
-          ;; ...or text color.
-          (add-face-text-property 0 (length FILENAME) '(:foreground "blue") t FILENAME)
-          (message "File %s does not exist." FILENAME))))))
+              (find-file flnm))
+          ;; If flnm does not exist, signal an error.
+          (error "File %s does not exist" flnm))))))
