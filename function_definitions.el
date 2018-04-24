b290fdf3e04c3b51b8f9555c1b3744254dd183cb
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-04-24 17:08:12 +0200

    Finished working on defun “gnus-user-format-function-a”.

--- function_definitions.el
+++ function_definitions.el
@@ -3,41 +3,36 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun gnus-user-format-function-a (hdr)
-  "If HDR is not a vector, return the string \"void\". Otherwise,
-if one of my addresses is contained in either the To: or the
-Cc: header or if both these headers are empty, return the From:
-header; else return the To: header or, if that is empty, the
-Cc: header. In all cases, prefer the full name contained in the
-header if present, otherwise use only the email address. See
-`gnus-summary-line-format' on how this function interfaces with
-Gnus."
-  (if (not (vectorp hdr))
+  "Return either the To: and Cc: header, or the From: header contained in
+vector HDR; do the former if the From: header either matches
+`my-email-addresses-regexp' or `my-email-addressees-regexp' or is empty; do
+the latter otherwise. In all cases, prefer the full name over the raw email
+address. Empty headers and To: and Cc: headers matching
+`my-email-undisclosed-recipients-regexp' are replaced with \"NA\". If HDR is
+not a vector, return \"void\". See `gnus-summary-line-format' on how this
+function interfaces with Gnus."
+  (if (null (vectorp hdr))
       "void"
-    (let ((from (mail-extract-address-components (mail-header-from hdr)))
-          (to (cdr (assoc 'To (mail-header-extra hdr))))
-          (cc (cdr (assoc 'Cc (mail-header-extra hdr))))
-          (ingorable-adresses-regexp "\\(\\(magic_willebinski\\)\\|\\(members\\)\\|\\(renke.vonseggern\\)\\)@gmx\\.\\(\\(de\\)\\|\\(net\\)\\)"))
-      (if (or (and (or (string= "" to) (null to))
-                   (or (string= "" cc) (null cc)))
-              (string-match-p ingorable-adresses-regexp (if (null to) "" to))
-              (string-match-p ingorable-adresses-regexp (if (null cc) "" cc)))
+    (let* ((from (nth 0 (mail-extract-address-components (mail-header-from hdr) t)))
+           (from-string (or (nth 0 from) (nth 1 from) "NA"))
+           (to-all (mail-extract-address-components (or (cdr (assoc 'To (mail-header-extra hdr))) "") t))
+           (cc-all (mail-extract-address-components (or (cdr (assoc 'Cc (mail-header-extra hdr))) "") t))
+           (to-cc-all (union to-all cc-all :test 'equal))
+           to-cc-all-format-string
+           (to-cc-all-string "NA"))
+      (if (>= (length to-cc-all) 1)
           (progn
-            (setq from (if (null (nth 0 from))
-                           (nth 1 from)
-                         (nth 0 from)))
-            from)
-        (setq to (mail-extract-address-components to))
-        (setq to (if (null (nth 0 to))
-                     (nth 1 to)
-                   (nth 0 to)))
-        (if (null to)
-            (progn
-              (setq cc (mail-extract-address-components cc))
-              (setq cc (if (null (nth 0 cc))
-                           (nth 1 cc)
-                         (nth 0 cc)))
-              cc)
-          to)))))
+            (setq to-cc-all-format-string (apply 'concat (make-list (length to-cc-all) "%s, ")))
+            (setq to-cc-all-format-string (substring to-cc-all-format-string 0 (- (length to-cc-all-format-string) 2)))
+            (setq to-cc-all-string (apply 'format to-cc-all-format-string (mapcar (lambda (sublist)
+                                                                                    (or (nth 0 sublist) (nth 1 sublist) "NA"))
+                                                                                  to-cc-all)))))
+      (or (null (string-match-p my-email-undisclosed-recipients-regexp to-cc-all-string)) (setq to-cc-all-string "NA"))
+      (if (or (string-match-p my-email-addresses-regexp from-string)
+              (string-match-p my-email-addressees-regexp from-string)
+              (string= "NA" from-string))
+          to-cc-all-string
+        from-string))))
 
 (defun my-abbrev-mode-on ()
   "Turn abbrev mode ON."
