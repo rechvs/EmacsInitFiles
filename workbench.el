cdcccde18199affd79c73b7322e6842886ea7f69
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-04-28 15:40:43 +0200

    defun “gnus-user-format-function-a”: To: and Cc: headers are now only processed if necessary.

--- workbench.el
+++ workbench.el
@@ -9,8 +9,7 @@ not a vector, return \"void\". See `gnus-summary-line-format' on how this
 function interfaces with Gnus."
   (if (null (vectorp hdr))
       "void"
-    (let* (
-           (match-data-old (match-data))
+    (let* ((match-data-old (match-data))
            (from (mail-header-from hdr))
            from-format-string
            (from-string "NA")
@@ -43,32 +42,33 @@ function interfaces with Gnus."
                   (setq from-format-string (apply 'concat (make-list (length from) "%s, ")))
                   (setq from-format-string (substring from-format-string 0 (- (length from-format-string) 2)))
                   (setq from-string (apply 'format from-format-string from))))
-            ;; Process concatenated To: and Cc: headers.
-            (setq to-cc-all (replace-regexp-in-string "[ 	]+" " " to-cc-all))
-            (while (or (string-match "\\(.*\"\\)\\([^,\"]+\\), \\([^,\"]+\\)\\(\".*\\)" to-cc-all)
-                       (string-match "\\(.*'\\)\\([^,']+\\), \\([^,']+\\)\\('.*\\)" to-cc-all))
-              (setq to-cc-all (concat (match-string 1 to-cc-all) (match-string 3 to-cc-all) " " (match-string 2 to-cc-all) (match-string 4 to-cc-all))))
-            (setq to-cc-all (mapcar (lambda (arg)
-                                      (setq arg (replace-regexp-in-string "^ +" "" arg))
-                                      (if (string-match "^\\(.+\\)[ 	]*<" arg)
-                                          (replace-regexp-in-string "^[\"'\\ 	]*\\|[\"'\\ 	]*$" "" (rfc2047-decode-string (substring arg (match-beginning 1) (match-end 1)) t))
-                                        (if (or (string-match email-address-regexp1 arg)
-                                                (string-match email-address-regexp2 arg))
-                                            (substring arg (match-beginning 1) (match-end 1))
-                                          nil)))
-                                    (split-string to-cc-all "," t)))
-            (setq to-cc-all (delete nil to-cc-all))
-            (if (>= (length to-cc-all) 1)
-                (progn
-                  (setq to-cc-all-format-string (apply 'concat (make-list (length to-cc-all) "%s, ")))
-                  (setq to-cc-all-format-string (substring to-cc-all-format-string 0 (- (length to-cc-all-format-string) 2)))
-                  (setq to-cc-all-string (apply 'format to-cc-all-format-string to-cc-all))))
-            (or (null (string-match-p my-email-undisclosed-recipients-regexp to-cc-all-string)) (setq to-cc-all-string "NA"))
-            ;; Return the appropriate string.
             (if (or (string-match-p my-email-addresses-regexp from-string)
                     (string-match-p my-email-addressees-regexp from-string)
                     (string= "NA" from-string))
-                to-cc-all-string
+                ;; If I was the sender or if the sender is missing, process and return concatenated To: and Cc: headers.
+                (progn
+                  (setq to-cc-all (replace-regexp-in-string "[ 	]+" " " to-cc-all))
+                  (while (or (string-match "\\(.*\"\\)\\([^,\"]+\\), \\([^,\"]+\\)\\(\".*\\)" to-cc-all)
+                             (string-match "\\(.*'\\)\\([^,']+\\), \\([^,']+\\)\\('.*\\)" to-cc-all))
+                    (setq to-cc-all (concat (match-string 1 to-cc-all) (match-string 3 to-cc-all) " " (match-string 2 to-cc-all) (match-string 4 to-cc-all))))
+                  (setq to-cc-all (mapcar (lambda (arg)
+                                            (setq arg (replace-regexp-in-string "^ +" "" arg))
+                                            (if (string-match "^\\(.+\\)[ 	]*<" arg)
+                                                (replace-regexp-in-string "^[\"'\\ 	]*\\|[\"'\\ 	]*$" "" (rfc2047-decode-string (substring arg (match-beginning 1) (match-end 1)) t))
+                                              (if (or (string-match email-address-regexp1 arg)
+                                                      (string-match email-address-regexp2 arg))
+                                                  (substring arg (match-beginning 1) (match-end 1))
+                                                nil)))
+                                          (split-string to-cc-all "," t)))
+                  (setq to-cc-all (delete nil to-cc-all))
+                  (if (>= (length to-cc-all) 1)
+                      (progn
+                        (setq to-cc-all-format-string (apply 'concat (make-list (length to-cc-all) "%s, ")))
+                        (setq to-cc-all-format-string (substring to-cc-all-format-string 0 (- (length to-cc-all-format-string) 2)))
+                        (setq to-cc-all-string (apply 'format to-cc-all-format-string to-cc-all))))
+                  (or (null (string-match-p my-email-undisclosed-recipients-regexp to-cc-all-string)) (setq to-cc-all-string "NA"))
+                  to-cc-all-string)
+              ;; Else, return the From: header.
               from-string))
         (set-match-data match-data-old)))))
   
@@ -104,3 +104,7 @@ function interfaces with Gnus."
              (string-match "\\(.*'\\)\\([^,']+\\), \\([^,']+\\)\\('.*\\)" to-cc-all))
     (setq to-cc-all (concat (match-string 1 to-cc-all) (match-string 3 to-cc-all) " " (match-string 2 to-cc-all) (match-string 4 to-cc-all))))
   to-cc-all)
+
+(if t
+    "true"
+  "false")
