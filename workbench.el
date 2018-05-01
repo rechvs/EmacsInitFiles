6a98eafd74988a5caf495006c20e1e5bd7296087
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-05-01 14:47:20 +0200

    defun “my-view-python-function-doc”: function now displays output of “help("FUNC")”; added error checking; TODO: make help buffer unmodified and read-only.

--- workbench.el
+++ workbench.el
@@ -4,37 +4,51 @@ Prompt for function name FUNC and use the subprocess to generate the documentati
 this is the output of either \"print(FUNC.__doc__)\", or, if that is empty, \"help(\"FUNC\")\".
   The documentation is then shown in buffer \"*help[Python3](FUNC)*\"."
   (interactive)
-  (let* ((proc-name "py3-doc-gen")
-         ;; (assoc-buf-name "*Python3 Documentation Generator*")
-         ;; (proc (get-process proc-name))
+  (let* ((cur-buf (current-buffer))
+         (proc-name "py3-doc-gen")
          (skippable-chars "a-z0-9")
          func
          (func-initial "")
          buf-name
          doc-command
+         (match-data-old (match-data))
          )
-
-    ;; (unless proc (setq proc (start-process proc-name assoc-buf-name "/usr/bin/python3")))
-    (if (and func (not (stringp func))) (error "Function name must be a string"))
-    ;; If the region is active, obtain function name from it.
-    (if (region-active-p)
-        (setq func-initial (buffer-substring-no-properties (region-beginning) (region-end)))
-      ;; If the region is not active, obtain the function name by scanning for text at point.
-      (let (p1 p2)
-        (save-excursion (skip-chars-backward skippable-chars (point-min))
-                        (setq p1 (point)))
-        (save-excursion (skip-chars-forward skippable-chars (point-max))
-                        (setq p2 (point)))
-        (setq func-initial (buffer-substring-no-properties p1 p2))))
-    ;; Prompt for function name
-    (setq func (completing-read "Function: " nil nil nil func-initial))
-    ;; Create help buffer if necessary. Display help buffer.
-    (setq buf-name (concat "*help[Python3](" func ")*"))
-    (setq doc-command (concat "print(" func ".__doc__)"))
-    (unless (get-buffer buf-name) (start-process proc-name buf-name "/usr/bin/python3" "-c" doc-command))
-    (unless (member buf-name (mapcar (lambda (window)
-                                       (buffer-name (window-buffer window)))
-                                     (window-list)))
-      (display-buffer buf-name))
-    
-    ))
+    (unwind-protect
+        (progn
+          (if (and func (not (stringp func))) (error "Function name must be a string"))
+          ;; If the region is active, obtain function name from it.
+          (if (region-active-p)
+              (setq func-initial (buffer-substring-no-properties (region-beginning) (region-end)))
+            ;; If the region is not active, obtain the function name by scanning for text at point.
+            (let (p1 p2)
+              (save-excursion (skip-chars-backward skippable-chars (point-min))
+                              (setq p1 (point)))
+              (save-excursion (skip-chars-forward skippable-chars (point-max))
+                              (setq p2 (point)))
+              (setq func-initial (buffer-substring-no-properties p1 p2))))
+          ;; Prompt for function name
+          (setq func (completing-read "Function: " nil nil nil func-initial))
+          ;; Generate and display help buffer if necessary.
+          (setq buf-name (concat "*help[Python3](" func ")*"))
+          (setq doc-command (concat "help(\"" func "\")"))
+          (unless (get-buffer buf-name)
+            (start-process proc-name buf-name "/usr/bin/python3" "-c" doc-command)
+            (while (get-process proc-name) (sleep-for 0.1)))
+          (set-buffer buf-name)
+          (goto-char (point-min))
+          (if (looking-at (concat "No Python documentation found for '" func "'"))
+              (error (buffer-substring-no-properties (match-beginning 0) (match-end 0)))
+            (while (re-search-forward (concat "\nProcess " proc-name " finished\n") (point-max) t)
+              (replace-match ""))
+            (goto-char (point-max))
+            (delete-blank-lines))
+          ;; TODO: make help buffer unmodified and read-only
+          (if (member buf-name (mapcar (lambda (window)
+                                         (buffer-name (window-buffer window)))
+                                       (window-list)))
+              (display-message-or-buffer (concat "Python documentation for " func " already displayed."))
+            (switch-to-buffer-other-window buf-name)
+            (goto-char (point-min))
+            (other-window 1)))
+      (set-match-data match-data-old)
+      (set-buffer cur-buf))))
