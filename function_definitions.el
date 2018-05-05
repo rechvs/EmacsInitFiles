aa7f510c3873ee8769763296425ce94b58d31c72
Renke Christian von Seggern <renke.vonseggern@stud.uni-goettingen.de>
2018-05-05 13:51:43 +0200

    “function_definitions.el”: added defun “my-view-python-object-help”.

--- function_definitions.el
+++ function_definitions.el
@@ -762,6 +762,63 @@ a prefix argument, select the previously unselected buffer."
                   (other-window 1)))))
       (message "There are more or less than two windows displayed."))))
 
+(defun my-view-python-object-help ()
+  "Prompt for a Python3 object name, using either the region or the text 
+around point as default. Display the output of \"help(\"OBJECT\")\" in a 
+dedicated buffer."
+  (interactive)
+  (let* ((cur-buf (current-buffer))
+         (proc-name "py3-doc-gen")
+         (skippable-chars "a-zA-Z0-9")
+         obj
+         (obj-def-input "")
+         buf-name
+         doc-command
+         (match-data-old (match-data)))
+    (unwind-protect
+        (progn
+          ;; If the region is active, obtain object name from it.
+          (if (region-active-p)
+              (setq obj-def-input (buffer-substring-no-properties (region-beginning) (region-end)))
+            ;; If the region is not active, obtain the object name by scanning for text at point.
+            (let (p1 p2)
+              (save-excursion (skip-chars-backward skippable-chars (point-min))
+                              (setq p1 (point)))
+              (save-excursion (skip-chars-forward skippable-chars (point-max))
+                              (setq p2 (point)))
+              (setq obj-def-input (buffer-substring-no-properties p1 p2))))
+          ;; Prompt for object name.
+          (setq obj (read-string (if (string= "" obj-def-input) "Object: " (concat "Object (default " obj-def-input "): ")) nil nil obj-def-input))
+          ;; Generate and display help buffer if necessary.
+          (setq buf-name (concat "*help[Python3](" obj ")*"))
+          (if (member buf-name (mapcar (lambda (window)
+                                         (buffer-name (window-buffer window)))
+                                       (window-list)))
+              (display-message-or-buffer (concat "Python documentation for " obj " already displayed."))
+            (setq doc-command (concat "help(\"" obj "\")"))
+            (unless (get-buffer buf-name)
+              (start-process proc-name buf-name "/usr/bin/python3" "-c" doc-command)
+              (while (get-process proc-name) (sleep-for 0.01))
+              (set-buffer buf-name)
+              (goto-char (point-min))
+              (if (looking-at (concat "No Python documentation found for '" obj "'"))
+                  (let ((error-message (buffer-substring-no-properties (match-beginning 0) (match-end 0))))
+                    (kill-buffer buf-name)
+                    (error "%s" error-message))
+                (while (re-search-forward (concat "\nProcess " proc-name " finished\n") (point-max) t)
+                  (replace-match ""))
+                (goto-char (point-max))
+                (delete-blank-lines))
+              (set-buffer-modified-p nil)
+              (read-only-mode)
+              (help-mode)                 ; Temporary fix until I have written a proper major mode for viewing (and quitting) Python help buffers.
+              (goto-char (point-min)))
+            (switch-to-buffer-other-window buf-name)
+            (other-window 1))
+          (if (region-active-p) (deactivate-mark)))
+      (set-match-data match-data-old)
+      (set-buffer cur-buf))))
+
 (defun my-visit-file-add-to-git-whitelist (&optional arg)
   "If called with prefix argument ARG, prompt for git repository directory,   
 otherwise use \"~/\" as default. Prompt for filename. If the file is   
