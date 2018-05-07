042b1c9ac57e729eb18c52093ff46441b78a2bb5
Renke Christian von Seggern <renke.vonseggern@gmx.de>
2018-05-07 17:17:36 +0200

    Finished working on “my-delete-parens-content”.

--- function_definitions.el
+++ function_definitions.el
@@ -211,6 +211,34 @@ If point is not on a blank line do nothing."
                (forward-line (- (/ ARG 2) 1))))))
       (message "Current line is not blank\."))))
 
+(defun my-delete-parens-content ()
+  "If a matching pair of opening and closing parentheses before and after 
+point is found, delete all text between it. \"Parentheses\" here means the 
+following characters: \"()\", \"[]\", and \"{}\"."
+  (interactive)
+  (let ((error-message-1 "No opening parenthesis found")
+        (error-message-2 "Not inside balanced parentheses")
+        parens-beg
+        opening-paren
+        closing-paren
+        (opening-parens-regexp "(\\|\\[\\|{")
+        (match-data-old (match-data)))
+    (unwind-protect
+        (save-excursion
+          (skip-chars-backward "^([{)]}" (point-min))
+          (setq parens-beg (point))
+          (goto-char (1- (point)))
+          (if (looking-at opening-parens-regexp)
+              (setq opening-paren (buffer-substring-no-properties (match-beginning 0) (match-end 0)))
+            (error error-message-1))
+          (setq closing-paren (cond ((string= "(" opening-paren) ")")
+                                    ((string= "[" opening-paren) "]")
+                                    ((string= "{" opening-paren) "}")))
+          (skip-chars-forward (concat "^" closing-paren) (point-max))
+          (or (looking-at (regexp-quote closing-paren)) (error error-message-2))
+          (delete-region parens-beg (point)))
+      (set-match-data match-data-old))))
+
 (defun my-elpy-shell-send-line (&optional arg)
   "Send the current line to the Python shell. Without prefix argument ARG, 
 move point to beginning of next relevant line of Python code (including empty 
