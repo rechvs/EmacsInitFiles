d1b6a80c4f724861457600b06f1eae1fd73b5a36
Renke Christian von Seggern <renke.vonseggern@gmx.de>
2018-05-08 07:30:49 +0200

    defun “my-delete-parens-content”: function now correctly travels forward across nested balanced parentheses.

--- workbench.el
+++ workbench.el
@@ -9,6 +9,7 @@ following characters: \"()\", \"[]\", and \"{}\"."
         opening-paren
         closing-paren
         (opening-parens-regexp "(\\|\\[\\|{")
+        (parens-level 0)
         (match-data-old (match-data)))
     (unwind-protect
         (save-excursion
@@ -21,7 +22,35 @@ following characters: \"()\", \"[]\", and \"{}\"."
           (setq closing-paren (cond ((string= "(" opening-paren) ")")
                                     ((string= "[" opening-paren) "]")
                                     ((string= "{" opening-paren) "}")))
-          (skip-chars-forward (concat "^" closing-paren) (point-max))
-          (or (looking-at (regexp-quote closing-paren)) (error error-message-2))
+          (goto-char (1+ (point)))
+          (skip-chars-forward (concat "^" opening-paren closing-paren) (point-max))
+          (catch 'here
+            (while t
+              (skip-chars-forward (concat "^" opening-paren closing-paren) (point-max))
+              (if (and (looking-at (regexp-quote closing-paren)) (= parens-level 0)) (throw 'here nil))
+              (if (looking-at (regexp-quote opening-paren))
+                  (progn
+                    (setq parens-level (1+ parens-level))
+                    (goto-char (1+ (point)))))
+              (if (looking-at (regexp-quote closing-paren))
+                  (progn
+                    (setq parens-level (1- parens-level))
+                    (goto-char (1+ (point))))) 
+              (if (= (point) (point-max)) (error error-message-2))))
           (delete-region parens-beg (point)))
       (set-match-data match-data-old))))
+
+
+
+(local-set-key (kbd "C-c SPC") 'my-delete-parens-content)
+
+\input{../LebenslaufDeutschModerncv01Praeambel.tex}
+
+% Color settings.
+\definecolor{MyUrlColor}{HTML}{008000} % Green
+
+% Signature.
+\newcommand{\MySignature}
+{\hspace{0.1em}\includegraphics{../../../../Bilder/Unterschrift01.png}}
+
+\input{../LebenslaufDeutschModerncv01Text.tex}
