0ac44be74a89f06b5357f96a4926fe35c63408b6
Renke Christian von Seggern <renke.vonseggern@gmx.de>
2018-05-08 08:35:28 +0200

    defun “my-delete-parens-content”: rewrote “while” for travelling across nested parentheses.

--- workbench.el
+++ workbench.el
@@ -1,7 +1,9 @@
 (defun my-delete-parens-content ()
-  "If a matching pair of opening and closing parentheses before and after 
-point is found, delete all text between it. \"Parentheses\" here means the 
-following characters: \"()\", \"[]\", and \"{}\"."
+  "Delete text between the opening and closing parentheses before and after 
+point. While looking for the opening parenthesis before point, do not travel 
+across closing parentheses. While looking for the matching closing 
+parenthesis, do travel across balanced parentheses. \"Parentheses\" here 
+means the following characters: \"()\", \"[]\", and \"{}\". "
   (interactive)
   (let ((error-message-1 "No opening parenthesis found")
         (error-message-2 "Not inside balanced parentheses")
@@ -23,34 +25,16 @@ following characters: \"()\", \"[]\", and \"{}\"."
                                     ((string= "[" opening-paren) "]")
                                     ((string= "{" opening-paren) "}")))
           (goto-char (1+ (point)))
-          (skip-chars-forward (concat "^" opening-paren closing-paren) (point-max))
-          (catch 'here
-            (while t
-              (skip-chars-forward (concat "^" opening-paren closing-paren) (point-max))
-              (if (and (looking-at (regexp-quote closing-paren)) (= parens-level 0)) (throw 'here nil))
-              (if (looking-at (regexp-quote opening-paren))
-                  (progn
-                    (setq parens-level (1+ parens-level))
-                    (goto-char (1+ (point)))))
+          (while (or (> parens-level 0) (null (looking-at (regexp-quote closing-paren))))
+            (if (= (point) (point-max)) (error error-message-2))
+            (if (looking-at (regexp-quote opening-paren))
+                (progn
+                  (setq parens-level (1+ parens-level))
+                  (goto-char (1+ (point))))
               (if (looking-at (regexp-quote closing-paren))
                   (progn
                     (setq parens-level (1- parens-level))
-                    (goto-char (1+ (point))))) 
-              (if (= (point) (point-max)) (error error-message-2))))
+                    (goto-char (1+ (point))))))
+            (skip-chars-forward (concat "^" opening-paren closing-paren) (point-max)))
           (delete-region parens-beg (point)))
       (set-match-data match-data-old))))
-
-
-
-(local-set-key (kbd "C-c SPC") 'my-delete-parens-content)
-
-\input{../LebenslaufDeutschModerncv01Praeambel.tex}
-
-% Color settings.
-\definecolor{MyUrlColor}{HTML}{008000} % Green
-
-% Signature.
-\newcommand{\MySignature}
-{\hspace{0.1em}\includegraphics{../../../../Bilder/Unterschrift01.png}}
-
-\input{../LebenslaufDeutschModerncv01Text.tex}
